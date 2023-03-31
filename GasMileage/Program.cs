e81c2c88a6ff49051b3e6c179f4fd59d9997304e
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-03-31 17:38:46 +0500

    solution done

--- GasMileage/Program.cs
+++ GasMileage/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class GasMileage
+{
+    static void Main(string[] args)
+    {
+        // declaring required varibales
+        int milesDriven = 0;
+        int gallonsUsedTankful;
+        int totalMilesDriven = 0;
+        int totalGallonsUsedTankful = 0;
+        float average;
+
+        while (milesDriven != -1)
+        {
+            // input miles driven
+            Console.Write("Enter Miles Driven (Or -1 To Exit):  ");
+            milesDriven = Convert.ToInt32(Console.ReadLine());
+
+            totalMilesDriven += milesDriven;
+
+            // input gallons used for tankful
+            Console.Write("Enter Gallons Used For Tankful:  ");
+            gallonsUsedTankful = Convert.ToInt32(Console.ReadLine());
+
+            // keeping record of total gallons used for tankful
+            totalGallonsUsedTankful += gallonsUsedTankful;
+
+            // calculating average
+            average = (float) milesDriven / gallonsUsedTankful;
+
+            // displaying mile per gallon
+            Console.WriteLine("Miles Per Gallon:  " + Math.Round(average, 2));
+        }
+
+        average = totalMilesDriven / totalGallonsUsedTankful;
+
+        // total miles per gallon
+        Console.WriteLine("Total Miles Per Gallon In This Trip:  " + average);
+    }
+}
